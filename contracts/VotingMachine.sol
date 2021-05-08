3cdd5f86cb47d8f129849d8a6b8e0d975a2d119a
Paulalex85 <tessier.paulalexandre@orange.fr>
2021-05-09 01:45:29 +0200

    can stake in voting machine

--- contracts/VotingMachine.sol
+++ contracts/VotingMachine.sol
@@ -1,25 +1,30 @@
 pragma solidity ^0.5.13;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "./utils/interfaces/IConfigurableRightsPool.sol";
+import "openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
+import "./utils/interfaces/BToken.sol";
 
-contract VotingMachine {
+contract VotingMachine is ReentrancyGuard {
     using SafeMath for uint256;
 
     mapping(address => uint256) private balances;
-    IConfigurableRightsPool     public  balancerPool;
+    BToken public balancerLPToken;
 
     event Staked(address indexed user, uint256 amount);
 
-    constructor(IConfigurableRightsPool _balancerPool) public {
-        require(_balancerPool != IConfigurableRightsPool(0));
-        balancerPool = _balancerPool;
+    constructor(BToken _balancerLPToken) public {
+        require(_balancerLPToken != BToken(0), "VotingMachine: LP token cannot be null");
+        balancerLPToken = _balancerLPToken;
     }
 
-//    function stake(uint256 amount) public nonReentrant {
-//        require(amount > 0, "VotingMachine: cannot stake 0");
-//        _balances[msg.sender] = _balances[msg.sender].add(_amount);
-//        balancerPool.transferFrom(msg.sender, address(this), amount);
-//        emit Staked(msg.sender, amount);
-//    }
+    function stake(uint256 amount) public nonReentrant {
+        require(amount > 0, "VotingMachine: cannot stake 0");
+        balances[msg.sender] = balances[msg.sender].add(amount);
+        balancerLPToken.transferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
 }
--- test/voting-machine.js
+++ test/voting-machine.js
@@ -1,9 +1,10 @@
-const { expect } = require('chai');
-const { constants, time, expectEvent } = require('@openzeppelin/test-helpers');
+const {expect} = require('chai');
+const {constants, time, expectEvent, expectRevert} = require('@openzeppelin/test-helpers');
 const helpers = require('./helpers');
 const BigNumber = require('bignumber.js');
+const VotingMachine = artifacts.require('VotingMachine');
 
-const { toWei } = web3.utils;
+const {toWei} = web3.utils;
 
 const deploy = async (accounts) => {
     // initialize test setup
@@ -29,10 +30,45 @@ contract('VotingMachine', (accounts) => {
 
     before('!! deploy setup', async () => {
         setup = await deploy(accounts);
+        stakeAmount = toWei('100');
+        halfStake = toWei('50');
+        quarterStake = toWei('25');
+        irregularStake = toWei('33');
+        irregularStake2 = toWei('76');
+        tinyStake = toWei('9');
     });
-    context('# contract ready', async () => {
-        it('balancer pool should be defined', async () => {
-            expect(await setup.votingMachine.balancerPool()).to.equal(setup.balancer.pool.address);
+    context('# contract initialization', async () => {
+        it('balancer LP token should be defined', async () => {
+            expect(await setup.votingMachine.balancerLPToken()).to.equal(setup.balancer.pool.address);
+        });
+        it('contract revert when balancer pool not defined', async () => {
+            await expectRevert(VotingMachine.new(constants.ZERO_ADDRESS), "VotingMachine: LP token cannot be null");
+        });
+    });
+    context('# stake lp token', async () => {
+        it('amount should be > 0', async () => {
+            await expectRevert(setup.votingMachine.stake(0), "VotingMachine: cannot stake 0");
+        });
+        it('should approve before stake', async () => {
+            await setup.balancer.pool.transfer(accounts[1], stakeAmount);
+            await expectRevert(setup.votingMachine.stake(stakeAmount, {from: accounts[1]}), "ERR_PCTOKEN_BAD_CALLER");
+        });
+        it('should approve enough', async () => {
+            await setup.balancer.pool.transfer(accounts[1], stakeAmount);
+            await setup.balancer.pool.approve(setup.votingMachine.address, halfStake, {from: accounts[1]});
+            await expectRevert(setup.votingMachine.stake(stakeAmount, {from: accounts[1]}), "ERR_PCTOKEN_BAD_CALLER");
+        });
+        it('can stake token', async () => {
+            await setup.balancer.pool.transfer(accounts[1], stakeAmount);
+            await setup.balancer.pool.approve(setup.votingMachine.address, stakeAmount, {from: accounts[1]});
+            let tx = await setup.votingMachine.stake(stakeAmount, {from: accounts[1]})
+            setup.data.tx = tx;
+
+            expect((await setup.votingMachine.balanceOf(accounts[1])).toString()).to.equal(stakeAmount);
+            await expectEvent.inTransaction(setup.data.tx.tx, setup.votingMachine, 'Staked', {
+                user: accounts[1],
+                amount: stakeAmount
+            });
         });
     });
 });
